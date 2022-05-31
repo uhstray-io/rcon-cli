19d1de73f0062ef29ecf5b23ee1c20df24994eeb
Joe Wisneski <joe.wisneski@wwt.com>
2022-05-31 07:22:41 -0400

    added windows files and update rcon.yaml

---
+++ rcon-0.10.2-win64/restart.bat
@@ -0,0 +1,6 @@
+bash 
+./rcon -a ksh.uhstray.io:25575 -p UhRisingF1! announcerestart 5
+./rcon -a ksh.uhstray.io -p UhRisingF1! announcerestart 2
+./rcon -a ksh.uhstray.io -p UhRisingF1! announcerestart 1
+./rcon -a ksh.uhstray.io -p UhRisingF1! announce "The server will restart in 30 seconds..."
+./rcon -a ksh.uhstray.io -p UhRisingF1! announce "The server will restart in 15 seconds..."
